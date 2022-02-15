10754d618cec98d1c1b64b3ab4f330baa0a8b74e
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2022-02-15 18:35:05 +0700

    [FIX] master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="{{ asset('template/assets/fonts/lineicons/font-css/LineIcons.css') }}">
 
     <!--====== Tailwind CSS ======-->
-    <link rel="stylesheet" href="{{ asset('template/assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/assets/css/tailwind.css') }}">
 
 </head>
 
